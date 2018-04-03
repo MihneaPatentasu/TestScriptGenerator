5037eba2e815bf8c591f5c085ad8e6fb85c5b5a2
MPatentasu <MPatentasu@SPATENTASU-PC.romania.luxoft.com>
2018-04-03 11:08:01 +0300

    -made a frontend draft with WindowBuilder (no functionalities yet)

--- src/backend/Main.java
+++ src/backend/Main.java
@@ -4,9 +4,6 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 
-import frontend.MainPanel;
-import frontend.SizedFrame;
-
 public class Main {
 
 	public static void main(String[] args) {
@@ -15,16 +12,16 @@ public class Main {
 //		paths.add("D:\\Useful info for modules\\PM");
 //		TestScriptGenerator.generateTestScript("D:\\Useful info for modules\\Test.py", paths);
 		
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				JFrame frame = new SizedFrame();
-				frame.setTitle("TestGenerator");
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.getContentPane().setLayout(null);
-				frame.add(new MainPanel());
-				frame.setVisible(true);
-			}
-		});
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				JFrame frame = new SizedFrame();
+//				frame.setTitle("TestGenerator");
+//				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//				frame.getContentPane().setLayout(null);
+//				frame.add(new MainPanel());
+//				frame.setVisible(true);
+//			}
+//		});
 	}
 
 }
